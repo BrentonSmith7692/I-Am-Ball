b7bc422f0fed967255706e131ffa6aea00a3cd3b
BrentonSmith7692 <31684106+BrentonSmith7692@users.noreply.github.com>
2017-09-06 16:19:00 +1000

    Add files via upload

---
+++ EnemyDeath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeath : MonoBehaviour {
+
+	public GameObject Turntable;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "PlayerBulletTag")
+			Destroy (gameObject);
+        else if (col.tag == "PlayerTag")
+            Application.LoadLevel(Application.loadedLevel);
+    }
+
+}
---
+++ PlayerBullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour 
+{
+	public float speed;
+	public GameObject LightningBolt;
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 30f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+	}
+	
+	void OnBecameInvisible () {
+		this.gameObject.SetActive(false);
+	}
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "EnemyTag")
+			Destroy(gameObject);
+	}
+}
